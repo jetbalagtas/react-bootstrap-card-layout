a27b7160946fc74905673f072fd6443252786478
Jet Balagtas <jetbalagtas@gmail.com>
2020-03-08 21:44:26 -0400

    condition and layout image prop, add id for key

--- src/App.js
+++ src/App.js
@@ -5,74 +5,82 @@ import CardList from "./components/CardList";
 const App = () => {
   const productCards = [
     {
+      id: 1,
       Heading: "tempor do tempor",
       Subheading: "ipsum et",
       Price: 22000
     },
     {
+      id: 2,
       Heading: "lorem",
       Subheading: "do incididunt",
       Price: 33000
     },
     {
+      id: 3,
       Heading: "ut magna ipsum",
       Subheading: "labore sed",
       Price: 14000
     },
     {
+      id: 4,
       Heading: "lorem incididunt elit",
       Subheading: "aliqua ipsum",
       Price: 43000,
       showBridge: true
     },
     {
+      id: 5,
       Heading: "adipiscing ipsum do et do elit eiusmod",
       Subheading: "et dolor",
       Price: 27000
     },
     {
+      id: 6,
       Heading: "magna lorem",
       Subheading: "eisumod do",
       Price: 15000
     },
     {
+      id: 7,
       Heading: "ipsum",
       Subheading: "elit elit",
       Price: 43000
     },
     {
+      id: 8,
       Heading: "sed eiusmod",
       Subheading: "consectetur dolor",
       Price: 25000
     },
     {
+      id: 9,
       Heading: "labore eiusmod elit ipsum aliqua sit lore ipsum",
       Subheading: "aliqua sed",
       Price: 37000,
       showBridge: true
     },
     {
+      id: 10,
       Heading: "aliqua ipsum sed ut elit",
       Subheading: "tempor aliqua",
       Price: 1000
     },
     {
+      id: 11,
       Heading: "aliqua",
       Subheading: "elit ut",
       Price: 4000
     },
     {
+      id: 12,
       Heading: "amet adipiscing",
       Subheading: "eisumod sit",
       Price: 23000
     }
   ];
 
-  return (
-    // <Container fluid="true" className="container">
-      <CardList productCards={productCards}/>
-    // {/* </Container> */}
-  );
+  return <CardList productCards={productCards} />;
 };
 
 export default App;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,31 +3,59 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "./CardList.css";
 
 const CardList = props => {
+  let cards = props.productCards.map(productCard => {
+    let img = "";
+    if (productCard.showBridge)
+      img = (
+        <div>
+          <Card.Img
+            variant="top"
+            className="imgBg"
+            src="https://res.cloudinary.com/bguggie/image/upload/v1425514882/darktiles_bg_gike55.jpg"
+          />
+          <Card.Img
+            variant="top"
+            className="imgTrue"
+            src="https://res.cloudinary.com/bguggie/image/upload/v1425514888/wide_ggbridge_bg_teneax.jpg"
+          />
+        </div>
+      );
+    else
+      img = (
+        <div>
+          <Card.Img
+            variant="top"
+            className="imgBg"
+            src="https://res.cloudinary.com/bguggie/image/upload/v1425514882/darktiles_bg_gike55.jpg"
+          />
+          <Card.Img
+            variant="top"
+            className="imgFg"
+            src="https://res.cloudinary.com/bguggie/image/upload/v1425514736/place_holder_zuvywg.png"
+          />
+        </div>
+      );
+    return (
+      <Card
+        key={productCard.id}
+        className="shadow"
+        style={{ width: "12rem", margin: "10px", borderRadius: "10px" }}>
+        {img}
+        <Card.Body>
+          <Card.Title>{productCard.Heading}</Card.Title>
+          <Card.Text>{productCard.Subheading}</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Card.Text className="productPrice">{productCard.Price}</Card.Text>
+        </Card.Footer>
+      </Card>
+    );
+  });
 
-  return (
-    <Container className="cardsContainer">
-      {props.productCards.map(productCard => {
-        return (
-          <Card
-            className="shadow"
-            style={{ width: "12rem", margin: "10px", borderRadius: "10px" }}>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-            <Card.Body>
-              <Card.Title>{productCard.Heading}</Card.Title>
-              <Card.Text>{productCard.Subheading}</Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Card.Text className="productPrice">{productCard.Price}</Card.Text>
-            </Card.Footer>
-          </Card>
-        );
-      })}
-    </Container>
-  );
+  return <Container className="cardsContainer">{cards}</Container>;
 };
 
 export default CardList;
