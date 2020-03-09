887d126295ed9dde1c895a49309b4110e8eef6c5
Jet Balagtas <jetbalagtas@gmail.com>
2020-03-09 15:29:58 -0400

    Updates

---
+++ precache-manifest.5b471bd448942f902c868b5799bdf29e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dd9898ed5e99b484633fedd742c338",
+    "url": "/react-bootstrap-card-layout/index.html"
+  },
+  {
+    "revision": "e9338ad9ee7e2784110f",
+    "url": "/react-bootstrap-card-layout/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "21ab21f1c1794d26240f",
+    "url": "/react-bootstrap-card-layout/static/css/main.93057d6d.chunk.css"
+  },
+  {
+    "revision": "e9338ad9ee7e2784110f",
+    "url": "/react-bootstrap-card-layout/static/js/2.e963f04d.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-bootstrap-card-layout/static/js/2.e963f04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ab21f1c1794d26240f",
+    "url": "/react-bootstrap-card-layout/static/js/main.f20ec7eb.chunk.js"
+  },
+  {
+    "revision": "9e360bdebf8f9bf14fbf",
+    "url": "/react-bootstrap-card-layout/static/js/runtime-main.a3632aa4.js"
+  }
+]);
